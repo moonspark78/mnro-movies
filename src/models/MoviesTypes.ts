da723b0790911ad84ad95831ce3c88654020d775
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-02-09 15:06:32 +0100

    Ajout des dossiers

---
+++ src/models/MoviesTypes.ts
@@ -0,0 +1,13 @@
+import { Categories } from "./CategoriesType";
+
+export interface MoviesType {
+
+    id: number;
+    name: string;
+    overview: string;
+    backdrop_path: string;
+    vote_average:number;
+    origin_country:string[];
+    poster_path:string;
+    genres:Categories;
+}
