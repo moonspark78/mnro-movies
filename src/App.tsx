042f54c00c3ba901d3247715f1c2b04e3e96785b
RiwaneAberkane <riwane.aberkane.pro@outlook.fr>
2023-02-17 15:03:17 +0100

    add route details

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Home } from './pages/Home';
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import Details from './pages/Details';
 
 function App() {
 
   return (
 
-      <Home/>
+    <Router>
+    <div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/details/:id" element={<Details/>} />
+    </Routes>
+    </div>
+  </Router>
 
   );
 }
